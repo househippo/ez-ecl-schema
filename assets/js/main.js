ce31c54723604110c7cc316f34af679c400ac0f8
Fujio Turner <fujio@househippo.com>
2015-11-23 22:53:12 -0700

    has DECIMAL & updated INT

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 function typeOf (obj) { return {}.toString.call(obj).split(' ')[1].slice(0, -1).toLowerCase();}
+function isDecimal(decimal1){return /^[-+]?[0-9]+\.[0-9]+$/.test(decimal1);}
+function isFloat(float1){}
 
 $(document).ready(function() {
     dropDownDataType();
@@ -14,7 +16,7 @@ function dropDownDataType(){
 
 //Push Button to Make Schema
 $(function() {
-    $("#make-ecl").click( function(){ readSampleData();});
+    $("#make-ecl").click( function(){readSampleData();});
 });
 
 function readSampleData(){
@@ -27,7 +29,7 @@ function readSampleData(){
     
     switch(dataType) {
         case 0:
-            csvParse(sampleData);
+            csvMakeECL(sampleData);
             break;
         case 1:
             alert('No JSON Yet');
@@ -42,28 +44,25 @@ function readSampleData(){
     $("#ecl-schema").css("background-color","#CEF6CE").slideDown(1750);
 }
 
-
-function csvParse(stringData){
-    var dataArray = Papa.parse(stringData,{dynamicTyping:true});
-     //console.log(dataArray);
-     csvMakeECL(dataArray);
-    //return CSVtoArray(stringData);
-}
-
-function csvMakeECL(csvJsArray){
-    
+function csvMakeECL(stringData){
+    var csvJsArray = Papa.parse(stringData,{dynamicTyping:true});
      var header_data = csvJsArray.data[0];
      //console.log(header_data);
     
      var header_counter = new Object();
       var header_counter_array = new Array();
+      
+      console.log(csvJsArray)
      
           $.each(csvJsArray.data[0],function(key,value){
               header_counter[value] = 0;
+              //console.log(key)
               var loop_array = new Object();
               loop_array["counter"] = 0;
               loop_array["numbers"] = 0;
               loop_array["strings"] = 0;
+              loop_array["numberType"] = null;
+              loop_array["stringType"] = null;
               header_counter_array[value] = loop_array;
           });
           
@@ -76,28 +75,61 @@ function csvMakeECL(csvJsArray){
                         //console.log(element_name_1)
                         //console.log(typeOf(value2));
                         var value_length = String(value2).length;
+                        
                         if(value_length > header_counter_array[element_name_1].counter){header_counter_array[element_name_1].counter = value_length;}
-                        if(typeOf(value2) === "string"){header_counter_array[element_name_1].strings = header_counter_array[element_name_1].strings + 1;}
-                        if(typeOf(value2) === "number"){header_counter_array[element_name_1].numbers = header_counter_array[element_name_1].numbers + 1;}
+                        var typeTest = String(typeOf(value2));
+                        //console.log(typeTest+" | "+ value2)
+                        if(typeTest === "string"){header_counter_array[element_name_1].strings = header_counter_array[element_name_1].strings + 1;}
+                        if(typeTest === "number"){
+                            header_counter_array[element_name_1].numbers = header_counter_array[element_name_1].numbers + 1;
+                            if(isDecimal(value2) && header_counter_array[element_name_1].numberType === null){
+                               header_counter_array[element_name_1].numberType = 'decimal';
+                            }
+                        }
             });
          }
      });
      
+     console.log(header_counter_array)
      var ecl = '';
      for(var a in header_counter_array){
          var type_ecl = 'STRING';
-         if(header_counter_array[a].strings === 0 && header_counter_array[a].numbers > 0){ type_ecl = 'INTEGER';}
+         if(header_counter_array[a].strings === 0 && header_counter_array[a].numbers > 0){
+             switch (header_counter_array[a].numberType){
+             case 'decimal':
+                type_ecl = 'DECIMAL';
+                ecl = ecl + '       ' + type_ecl + variableLengthCheck(header_counter_array[a].counter) + '  '+ cleanUpName(a) + '; \r\n';
+                break;
+            default:
+            case 'interger':
+               type_ecl = 'INTEGER';
+               ecl = ecl + '       ' + type_ecl + integerSize(header_counter_array[a].counter) + '  '+ cleanUpName(a) + '; \r\n';
+               break;  
+            }   
+         }
          ecl = ecl + '       ' + type_ecl + variableLengthCheck(header_counter_array[a].counter) + '  '+ cleanUpName(a) + '; \r\n';
      }     
     ecl = ecl_bookends($('#schema-name').val(),ecl);
     $('#ecl-schema').val(ecl);
 }
 
-
 function variableLengthCheck(number){
     if(number === 0){return '{???}';}else{ return number;}
 }
 
+function integerSize(size){  
+    var size = Math.abs(size);
+    if(size <= 128){ return 1;}
+    if(size <= 32768){ return 2;}
+    if(size <= 8388608){ return 3;}
+    if(size <= 2147483648){ return 4;}
+    if(size <= 549755813887){ return 5;}
+    if(size <= 140737488355327){ return 6;}
+    if(size < 140737488355327){ return 8;}
+}
+
+
+///////////////////JSON///////////////////////////
 function jsonMakeECL(json){
     var obj = jQuery.parseJSON(json);
     
