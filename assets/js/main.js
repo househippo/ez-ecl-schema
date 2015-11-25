3936f9088859c6946a55c681c49d37e78601e53f
Fujio Turner <fujio@househippo.com>
2015-11-24 23:43:22 -0700

    correct INT value

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,17 @@ function isFloat(float1){}
 
 debug = false;
 
-$(document).ready(function() {
-    dropDownDataType();
-});
-
+$(document).ready(function(){ dropDownDataType(); });
 
 function dropDownDataType(){
-    var types = [{"type":"CSV","message":"Put in your header + a few hundred records to make a quick schema","default":true,"sample":""},{"type":"JSON","message":"Not Yet","default":false},{"type":"XML (Soon)","message":"Not Yet","default":false}];
+    var types = [{"type":"CSV","message":"Put in your header + a few hundred records to make a quick schema","default":true,"sample":""},{"type":"JSON (Coming Soon)","message":"Not Yet","default":false},{"type":"XML (Coming Soon)","message":"Not Yet","default":false}];
     $.each(types,function( key, value ) {
         $('#data-type').append($("<option></option>").attr("value",key).text(value.type));
     });
 }
 
 //Push Button to Make Schema
-$(function() {
-    $("#make-ecl").click( function(){readSampleData();});
-});
+$(function() { $("#make-ecl").click( function(){readSampleData();});});
 
 function readSampleData(){
     var dataType = parseInt($('#data-type').val());
@@ -54,13 +49,14 @@ function csvMakeECL(stringData){
       if(debug === true){console.log(csvJsArray);}
           $.each(csvJsArray.data[0],function(key,value){
               header_counter[value] = 0;
-              //console.log(key)
+
               var loop_array = new Object();
               loop_array["counter"] = 0;
               loop_array["numbers"] = 0;
               loop_array["strings"] = 0;
               loop_array["numberType"] = null;
               loop_array["stringType"] = null;
+              loop_array["maxIntValue"] = 0;
               header_counter_array[value] = loop_array;
           });
           
@@ -74,37 +70,44 @@ function csvMakeECL(stringData){
                         
                         if(value_length > header_counter_array[element_name_1].counter){header_counter_array[element_name_1].counter = value_length;}
                         var typeTest = String(typeOf(value2));
-                        //console.log(typeTest+" | "+ value2)
                         if(typeTest === "string"){header_counter_array[element_name_1].strings = header_counter_array[element_name_1].strings + 1;}
                         if(typeTest === "number"){
                             header_counter_array[element_name_1].numbers = header_counter_array[element_name_1].numbers + 1;
+                            
                             if(isDecimal(value2) && header_counter_array[element_name_1].numberType === null){
                                header_counter_array[element_name_1].numberType = 'decimal';
                             }
+                           if( header_counter_array[element_name_1].maxIntValue < value2){header_counter_array[element_name_1].maxIntValue = value2; } 
+                                
                         }
             });
          }
      });
      
      if(debug === true){console.log(header_counter_array);}
+     
+     var check_number = false;
+     if($("#no_numbers").is(':checked')){check_number = true;}
+     
      var ecl = '';
+     var counter = 1;
+     
      for(var a in header_counter_array){
-         
          if(debug === true){console.log(a);}
          var type_ecl = '';
          if(header_counter_array[a].strings === 0 && header_counter_array[a].numbers > 0){
            if(header_counter_array[a].numberType === 'decimal'){
                 type_ecl = 'DECIMAL';
-                ecl = ecl + '       ' + type_ecl + variableLengthCheck(header_counter_array[a].counter) + '  '+ cleanUpName(a) + '; \r\n';
+                ecl = ecl + '       ' + type_ecl + variableLengthCheck(header_counter_array[a].counter) + '  '+ cleanUpName(a) + ';'+showNumber(counter,check_number)+'\r\n';
             }else{
                type_ecl = 'INTEGER';
-               ecl = ecl + '       ' + type_ecl + integerSize(header_counter_array[a].counter) + '  '+ cleanUpName(a) + '; \r\n'; 
+               ecl = ecl + '       ' + type_ecl + integerSize(header_counter_array[a].maxIntValue) + '  '+ cleanUpName(a) + ';'+showNumber(counter,check_number)+'\r\n';
             }
          }else{
          type_ecl = 'STRING';
-         ecl = ecl + '       ' + type_ecl + variableLengthCheck(header_counter_array[a].counter) + '  '+ cleanUpName(a) + '; \r\n';
+         ecl = ecl + '       ' + type_ecl + variableLengthCheck(header_counter_array[a].counter) + '  '+ cleanUpName(a) + ';'+showNumber(counter,check_number)+'\r\n';
          }
-         
+         counter++;
      }     
     ecl = ecl_bookends($('#schema-name').val(),ecl);
     $('#ecl-schema').val(ecl);
@@ -123,6 +126,7 @@ function integerSize(size){
     if(size < 140737488355327){ return 8;}
 }
 
+function showNumber(number,status){ if(status){return   '   //'+number+' ';}else{return '';} }
 
 ///////////////////JSON///////////////////////////
 function jsonMakeECL(json){
@@ -134,31 +138,26 @@ function jsonMakeECL(json){
     if(debug === true){console.log(typeOf(obj[a]));}
     
     if(typeOf(obj[a]) === "string"){
-        
         var l_array = new Array();
-        
         l_array["Type"] = "string";
         l_array["Size"] = obj[a].length;             
         j_array[a] = l_array;
-
     }
       if(typeOf(obj[a]) === "number"){
-          
         var l_array = new Array();
         l_array["Type"] = "number";
         var value1 = String(obj[a]);
         l_array["Size"] = value1.length;             
         j_array[a] = l_array;
       }
-    
     }
-    console.log(j_array)
+    console.log(j_array);
 }
 
 function cleanUpName(string){
     //string = string.toLowerCase().replace(/^(.)|\s(.)/g, function($1) { return $1.toUpperCase(); });
-    //string = string.replace(/ /g, '_');
-    string = string.replace(/ /g, '_').toLowerCase();
+    string = string.replace(/ /g, '_');
+    //string = string.replace(/ /g, '_').toLowerCase();
     return string.replace(/[^A-Za-z_0-9]/g,''); //except letters & "_"
 }
 
